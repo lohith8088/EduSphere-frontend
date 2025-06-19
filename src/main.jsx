211fa2049fd43d89f4d973764fd4cde3664f5d4d
Lohith L P <149100284+lohith8088@users.noreply.github.com>
2025-06-19 08:42:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:5000";
+export const server = "https://edusphere-server-81eg.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
